ddf4fdc24a479df2dfa4d4b4c5b7353bf224eb67
Daryl_Moh <Daryl.Moh@hotmail.com>
2022-12-16 16:52:03 +0800

    second commit for SDF assessment

--- task01/src/mailmerge/Main.java
+++ task01/src/mailmerge/Main.java
@@ -14,6 +14,9 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         
+        // I managed to get the map of input information from reading the CSV 
+        // Then I got stuck at trying to find how to compare the keys and replace the items in the output
+
         // Put file names into a string
         String csv = args[0]; 
         String template = args[1];
@@ -50,31 +53,37 @@ public class Main {
         String line2;
         //List<String> keyList = new LinkedList<String>();
 
-        while ((line2=tempbr.readLine()) != null) {
-            
-            //System.out.println(line2);
-            // Temp storage for combined string
-            String joinedString;
-            String[] temp = line2.split(" ");
-            
-            // Get keyList from the template file to check and replace the words during printing
-            for(Integer i=0; i<temp.length; i++){
-                
-                if(temp[i].contains("<")) {
-                    String key;
-                    key = temp[i].replaceAll("[^a-zA-Z0-9]", "");
-                    
-                    stringb.append();  
-                    
+        for (HashMap m : customer) {
 
-                } else {
+            while ((line2=tempbr.readLine()) != null) {
+            
+                //System.out.println(line2);
+                // Temp storage for combined string
+                String joinedString;
+                String[] temp = line2.split(" ");
+                
+                // Get keyList from the template file to check and replace the words during printing
+                for(Integer i=0; i<temp.length; i++){
+                    
+                    if(temp[i].contains("<")) {
+                        String key;
+                        key = temp[i].replaceAll("[^a-zA-Z0-9]", "");
     
-                    stringb.append(temp[i] + " ");
-
+                        //stringb.append();  
+    
+                        // Idea is trying to write each array into a string and replacing the correct information when it reaches it
+                        // But I cannot figure out how to get it work
+    
+                    } else {
+        
+                        stringb.append(temp[i] + " ");
+    
+                    }
+                   
+                    
                 }
-               
-                
-            }
+            
+        }
 
             // 
                     
